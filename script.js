aeb64683a5c335b80e63089e72b80d5e9be09ba8
ivandamian1 <adroverivan7@gmail.com>
2024-10-24 22:34:14 -0400

    updated js and html

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
-const sundayInput = number.querySelector('#sunday-input')
-const mondayInput = number.querySelector('#monday-input')
+const sundayInput = document.querySelector('#Sunday')
+const mondayInput = document.querySelector('#Monday')
+const tuesdayInput = document.querySelector('#Tuesday')
+const wednesdayInput = document.querySelector('#Wednesday')
+const thursdayInput = document.querySelector('#Thursday')
+const fridayInput = document.querySelector('#Friday')
+const saturdayInput = document.querySelector('#Saturday')
+const submitButton = document.querySelector('#submit')
 
-document.getElementById("sunday").innerHTML = 
-submitButton.addEventListener('click'), function (event) {
+submitButton.addEventListener('click', function (event) {
     event.preventDefault();
-}
 
+    const sunday = parseInt(sundayInput.value,10);
+    const monday = parseInt(mondayInput.value,10);
+    const tuesday = parseInt(tuesdayInput.value,10);
+    const wednesday = parseInt(wednesdayInput.value,10);
+    const thursday = parseInt(thursdayInput.value,10);
+    const friday = parseInt(fridayInput.value,10);
+    const saturday = parseInt(saturdayInput.value,10);
+  
+      localStorage.setItem('sunday', sunday);
+      localStorage.setItem('monday', monday);
+      localStorage.setItem('tuesday', tuesday);
+      localStorage.setItem('wednesday', wednesday);
+      localStorage.setItem('thursday', thursday);
+      localStorage.setItem('friday', friday);
+      localStorage.setItem('saturday', saturday);
+    
 //TODO coding for total hours / 7 for average hours per week
+
+const totalHours = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]
+
+console.log (totalHours)
+
+    // TODO: Calculate and display the average salary
+  
+    const addedHours = sunday + monday + tuesday + wednesday + thursday + friday + saturday
+
+    console.log (addedHours)
+
+  
+  const averageHours = addedHours / 7;
+  
+  console.log(`Your average number of hours slept per night is ${averageHours.toFixed(2)}`);
+  
+  }
+)
+
+
